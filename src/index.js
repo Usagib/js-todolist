19384273a04d6ad47741ca016faed12b6c857de0
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-05-20 16:11:03 -0500

    Object created: project task
    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,96 @@
 // import stylesheets
 import './style.css';
 
-const saveLocalAndRender = () => {
-
-}
-
-const project = (name) => {
-  const pName = name;
+const project = (pName) => {
+  const name = pName;
   let taskArr = [];
 
   const addTask = (Arr, task) => {
+  }
+
+  const deleteTask = () => {
 
   }
 
   const setName = (newName) => {
-    this.pName = newName
+    this.name = newName;
   }
 
   const getName = () => {
-    return this.pName
+    return this.name;
   }
+
+  return {
+    addTask,
+    deleteTask,
+    setName,
+    getName,
+    taskArr
+  };
+
 }
+
+const task = (tName, tDescription, tDate, tChecked) => {
+  const name = tName;
+  const description = tDescription;
+  const date = tDate;
+  const checked = tChecked;
+
+  const setName = (newName) => {
+    this.name = newName;
+  }
+
+  const getName = () => {
+    return this.name;
+  }
+
+  const setDescription = (newDescription) => {
+    this.description = newDescription;
+  }
+
+  const getDescription = () => {
+    return this.description;
+  }
+
+  const setDate = (newDate) => {
+    this.date = newDate;
+  }
+
+  const getDate = () => {
+    return this.date;
+  }
+
+  const changeChecked = () => {
+    this.checked = !this.checked;
+  }
+
+  const getChecked = () => {
+    return this.checked;
+  }
+
+  return {
+    setName,
+    setDescription,
+    setDate,
+    changeChecked,
+    getName,
+    getDescription,
+    getDate,
+    getChecked
+  };
+}
+
+const render = () => {
+
+}
+
+const saveLocalAndRender = () => {
+  localStorage.setItem('LocalTaskArray', JSON.stringify(myLibrary));
+  render();
+}
+
+const test = () => {
+  let newProject = project('restaurant page');
+}
+
+test();
