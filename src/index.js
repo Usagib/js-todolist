3f0dfc7cf4e4a8a6e7418e5154958b2a26b7fdfe
firmo holanda <firmo@viize.com>
2020-05-25 17:19:00 -0300

    create addProject

--- src/index.js
+++ src/index.js
@@ -26,17 +26,62 @@ const btnAddTodo = document.getElementById('addTodo');
 btnAddTodo.onclick = () => { addTodo(); };
 
 function addProject() {
-  const projectTitle = document.getElementByid('new-project-title').value;
+  const projectTitle = document.getElementById('new-project-title').value;
   const newProject = new Project(projectTitle);
   projectList.push(newProject);
+
+  renderProjects(projectList);
 }
+
+
+function removeProject(project) {
+  projectList.splice((project.id - 1), 1);
+  alert(project.title + " deleted")
+  renderProjects(projectList);
+}
+
 
 function addTodo() {
 
 }
 
 function renderProjects(projectList) {
-  addProject();
+  const projectTable = document.getElementById('project-table');
+  projectTable.innerHTML = '';
+
+  projectList.forEach((project) => {
+    const tableRow = projectTable.insertRow();
+
+    const indexCol = document.createElement('th');
+    indexCol.innerHTML = project.id;
+
+    tableRow.appendChild(indexCol);
+
+    const titleCol = tableRow.insertCell(1);
+    titleCol.innerHTML = project.title;
+    titleCol.style.cursor = "pointer";
+    titleCol.addEventListener('click', function () {
+      renderTodos(projectList[project.id - 1])
+    });
+
+    const editCol = tableRow.insertCell(2);
+    const editButton = document.createElement('button');
+    editButton.innerText = "edit";
+    editButton.classList.add("btn", "btn-outline-secondary", "btn-sm");
+    editButton.addEventListener('click', function (e) {
+      //renderProjects(projectList);
+    });
+    editCol.appendChild(editButton);
+
+    const deleteCol = tableRow.insertCell(3);
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.classList.add("btn", "btn-outline-danger", "btn-sm");
+    removeButton.addEventListener('click', function (e) {
+      removeProject(project);
+    });
+    deleteCol.appendChild(removeButton);
+  });
 }
 
 function renderTodos(project) {
