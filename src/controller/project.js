b7ccfa488289ef064d42d8d275aa2327d62175f2
firmo holanda <firmo@viize.com>
2020-05-25 16:42:49 -0300

    refactor project and todo classes

--- src/controller/project.js
+++ src/controller/project.js
@@ -1,10 +1,10 @@
-Class Project {
-  constructor(title, todos =[]) {
-    this.contructor.counter = (this.constructor.counter || 0) +1;
+class Project {
+  constructor(title, todos = []) {
+    this.constructor.counter = (this.constructor.counter || 0) + 1;
     this.id = this.constructor.counter;
     this.title = title;
     this.todos = todos;
   }
 }
 
-export default Project
+export default Project;
--- src/controller/todo.js
+++ src/controller/todo.js
@@ -1,6 +1,6 @@
-Class Todo {
-  constructor(title, dueDate, pripority, description) {
-    this.contructor.counter = (this.constructor.counter || 0) +1;
+class Todo {
+  constructor(title, dueDate, priority, description) {
+    this.constructor.counter = (this.constructor.counter || 0) + 1;
     this.id = this.constructor.counter;
     this.title = title;
     this.dueDate = dueDate;
