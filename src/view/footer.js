bd4383be164e8173e0994ccd190f338a9f329439
firmo holanda <firmo@viize.com>
2020-05-25 16:12:41 -0300

    create footer.js

---
+++ src/view/footer.js
@@ -0,0 +1,13 @@
+const footer =
+
+  `
+<footer class="footer">
+  <hr>
+  <small>
+    todo-list-js-webpack created by:
+    <a href="https://github.com/firmoholanda">firmo holanda</a>
+  </small>
+</footer>
+`
+
+export default footer;
