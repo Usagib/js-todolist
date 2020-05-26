622e77ebd1bf7e5e841d6bab6fab77d123541aa7
firmo holanda <firmo@viize.com>
2020-05-26 18:21:13 -0300

    change name order on footer

--- src/view/footer.js
+++ src/view/footer.js
@@ -1,12 +1,13 @@
 const footer =
 
-  `
+`
 <footer class="footer">
   <hr>
   <small>
     todo-list-js-webpack created by:
-    <a href="https://github.com/firmoholanda">firmo holanda</a> and
     <a href="https://github.com/usagib">Eduardo Bonilla</a>
+    and
+    <a href="https://github.com/firmoholanda">firmo holanda</a> 
   </small>
 </footer>
 `
